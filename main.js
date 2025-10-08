47150975b4e1f18a224d6d54661d7617ff8299b9
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-08 11:44:07 +0200

    Created and mounted app

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+
+const app = Vue.createApp({
+    data :function(){
+        return {
+            product : 'Socks',
+            discription: 'colord'
+            
+        }
+
+    }
+})
