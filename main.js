6ff2bd6fe6e7204acd9131d9d63d0248b9233d25
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-08 12:19:04 +0200

    Added inventory status

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
-            
+            image: './assets/images/socks_blue.jpg',
+            inVentory: 100,
         }
     }
 })
