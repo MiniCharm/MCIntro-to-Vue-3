13da22c19679ae15916942362894be00d8ef9340
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-09 10:02:56 +0200

    Added display of list with details and display of array containing sizes

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id:1,color:'green'},
+                {id:2,color:'blue'}
+            ],
+            sizes: [
+                {id:1,size:'38'},
+                {id:2,size:'40'},
+                {id:3,size:'42'},
+                {id:4,size:'45'}
+            ]
         }
     }
 })
